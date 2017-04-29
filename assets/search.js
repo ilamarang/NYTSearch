75950538d10ccad2c0b87eb495538686957a1c43
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-04-29 13:30:43 -0400

    Framework committed

---
+++ assets/search.js
@@ -0,0 +1,21 @@
+$("#search").on("click",function() {
+
+	if($("#searchTerm").val() != "")
+	{
+		var searchTerm = $("#searchTerm").val();	
+	}
+	
+	if($("#searchStYr").val() != "")
+	{
+		var searchStYr = $("#searchStYr").val();	
+	}
+	if($("#searchEdYr").val() != "")
+	{
+		var searchEdYr = $("#searchEdYr").val();	
+	}
+	if($("#searchNumOfRds").val() != "")
+	{
+		var searchNumOfRds = $("#searchNumOfRds").val();	
+	}
+
+});
